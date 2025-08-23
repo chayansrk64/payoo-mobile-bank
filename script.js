dade907744ba8039d54166e73f3e7ea2db0874b7
chayansrk64 <chayansrk64@gmail.com>
2025-08-23 13:44:02 +0600

    home.html created

---
+++ script.js
@@ -0,0 +1,21 @@
+// login button event
+document.getElementById('login-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    
+    const moblileNumber = 12345678910;
+    const pinNumber = 1234;
+
+    const numberInput = document.getElementById('mobile-number').value;
+    const numberInputConverted = parseInt(numberInput);
+
+    const pinInput = document.getElementById('pin-number').value;
+    const pinInputConverted = parseInt(pinInput);
+    
+    if(moblileNumber === numberInputConverted && pinNumber === pinInputConverted){
+        window.location.href = './home.html'
+    }else{
+        alert('Invalid Credentials')
+    }
+
+
+})
